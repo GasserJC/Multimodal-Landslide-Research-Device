0d0773fbd962280f98868e56793ea4dc56741139
Jacob Gasser <jacobgasser17@gmail.com>
2021-01-20 13:19:02 -0600

    init

--- PySense.py
+++ PySense.py
@@ -11,6 +11,8 @@ from datetime import datetime
 import asyncio
 import PySense
 
+#testing 2
+
 cred = credentials.Certificate('creds.json') #need IAM credentials file.
 firebase_admin.initialize_app(cred)
 db = firestore.client()
